f775c5968c475aad937d22dc864b9a738cdade5e
RodAbreu <abreu.rodrigo97@gmail.com>
2018-10-17 14:32:50 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/rodrigo/contatinho/Contatinho.kt
@@ -0,0 +1,6 @@
+package com.example.rodrigo.contatinho
+
+data class Contatinho (val nome: String,
+                       val telefone: String,
+                       val email: String? = null,
+                       val endereco: String? = null)
---
+++ app/src/main/java/com/example/rodrigo/contatinho/ListaContatinhosActivity.kt
@@ -0,0 +1,71 @@
+package com.example.rodrigo.contatinho
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_lista_contatinhos.*
+import java.util.ArrayList
+
+class ListaContatinhosActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CADASTRO: Int = 1
+        private const val LISTA = "ListaContatinhos"
+    }
+    var listaContatinhos: MutableList<String> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_contatinhos)
+
+        carregaLista()
+
+        btnAddContatinho.setOnClickListener() {
+            val cadastrarContatinho = Intent(this, CadastraContatinhoActivity::class.java)
+            startActivityForResult(cadastrarContatinho, REQUEST_CADASTRO)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        carregaLista()
+    }
+
+    private fun carregaLista() {
+        val adapter = ContatinhoAdapter(listaContatinhos)
+        val layoutManager = LinearLayoutManager(this)
+        val dividerItemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+
+        rvContatinhos.adapter = adapter
+        rvContatinhos.layoutManager = layoutManager
+        rvContatinhos.addItemDecoration(dividerItemDecoration)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_CADASTRO && resultCode == Activity.RESULT_OK) {
+            val novoContatinho: String? = data?.getStringExtra(CadastraContatinhoActivity.NOME_CONTATINHO)
+            if (novoContatinho != null) {
+                listaContatinhos.add(novoContatinho)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putStringArrayList(LISTA, listaContatinhos as ArrayList<String>)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        if(savedInstanceState != null){
+            listaContatinhos = savedInstanceState.getStringArrayList(LISTA).toMutableList();
+        }
+    }
+}
+
